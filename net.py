e012871e8dfe60d0f91c939c59ac457c9e6a9896
mani1911 <mail2aj19@gmail.com>
2022-11-24 11:51:36 +0530

    graphs

--- net.py
+++ net.py
@@ -1,5 +1,6 @@
 import networkx
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 G = networkx.Graph()
 
@@ -8,9 +9,18 @@ for line in f:
   line = line.split('-')
   G.add_edge(int(line[0]), int(line[1]), weight=int(line[2]))
 
-pos = networkx.spring_layout(G, k=10)
+pos = networkx.spring_layout(G, k=100)
 weight_labels = networkx.get_edge_attributes(G,'weight')
-networkx.draw(G, pos, font_color = 'pink', node_shape = 's',with_labels=True)
+options = {
+  "font_size": 20,
+  "node_size": 1000,
+  "node_color": "blue",
+  "edge_color": "black",
+  "linewidths": 2,
+  "node_shape": 'o',
+  "width": 1,
+}
+networkx.draw(G, pos, **options,with_labels=True)
 
 networkx.draw_networkx_edge_labels(G,pos,edge_labels=weight_labels)
 plt.savefig("pic/mg.png")
@@ -22,14 +32,24 @@ f = open('txt/mst')
 for line in f:
   line = line.split('-')
   G.add_edge(int(line[0]), int(line[1]), weight=int(line[2]))
-
-
-pos = networkx.spring_layout(G, k=10)
-weight_labels = networkx.get_edge_attributes(G,'weight')
-networkx.draw(G, pos, font_color = 'pink', node_shape = 's',with_labels=True)
-
-networkx.draw_networkx_edge_labels(G,pos,edge_labels=weight_labels)
-plt.savefig("pic/mst.png")
-plt.show()
+  pos = networkx.spring_layout(G, k=100)
+  weight_labels = networkx.get_edge_attributes(G,'weight')
+  options = {
+    "font_size": 20,
+    "node_size": 1000,
+    "node_color": "blue",
+    "edge_color": "black",
+    "linewidths": 2,
+    "node_shape": 'o',
+    "width": 1,
+  }
+  networkx.draw(G, pos, **options,with_labels=True)
+  networkx.draw_networkx_edge_labels(G,pos,edge_labels=weight_labels)
+  plt.savefig("pic/mst.png")
+  plt.show(block=False)
+  plt.pause(3)
+  plt.close()
+image = mpimg.imread("pic/mst.png")
+plt.imshow(image)
 G.clear()
 f.close()
